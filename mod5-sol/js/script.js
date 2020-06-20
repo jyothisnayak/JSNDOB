06463b1df7f603d3b3c8339c2634674c2ceaf378
HP <jyothinayak.cse@bmsce.ac.in>
2020-06-20 22:55:31 +0530

    update

--- mod5-sol/js/script.js
+++ mod5-sol/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   menuItemsUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowMenuItemsHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
