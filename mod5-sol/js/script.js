d08b68a36762b1211116f2e48b3875e4fd7893e6
HP <jyothinayak.cse@bmsce.ac.in>
2020-06-20 22:44:07 +0530

    update1

--- mod5-sol/js/script.js
+++ mod5-sol/js/script.js
@@ -97,13 +97,28 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
         switchMenuToActive();
-        var chooseRandomCategory=chooseRandomCategory(categories);
-        var short_name=chooseRandomCategory.short_name;
+        categoriesTitleHtml,
+    function (categoriesTitleHtml) {
+      // Retrieve single category snippet
+      $ajaxUtils.sendGetRequest(
+        categoryHtml,
+        function (categoryHtml) {
+          // Switch CSS class active to menu button
+          switchMenuToActive();
+
+          var categoriesViewHtml =
+            buildCategoriesViewHtml(categories,
+                                    categoriesTitleHtml,
+                                    categoryHtml);
+          insertHtml("#main-content", categoriesViewHtml);
+        //var chooseRandomCategory();
+       //chooseRandomCategory(categories);
+        //var short_name=chooseRandomCategory.short_name;
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName =insertProperty(chosenCategoryShortName,
-                   "short_name",short_name);
+      //var chosenCategoryShortName =insertProperty(chosenCategoryShortName,
+      //             "short_name",short_name);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -116,9 +131,9 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = buildAndShowCategoriesHTML(chosenCategoryShortName);
+      //var homeHtmlToInsertIntoMainPage = buildAndShowCategoriesHTML(chosenCategoryShortName);
 
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      //insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
