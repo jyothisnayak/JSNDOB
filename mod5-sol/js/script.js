1c37f85db1e3e9e42638728ed2f69d9a6eac2c12
HP <jyothinayak.cse@bmsce.ac.in>
2020-06-21 15:12:04 +0530

    use

--- mod5-sol/js/script.js
+++ mod5-sol/js/script.js
@@ -128,7 +128,7 @@ function buildAndShowHomeHTML (categories) {
       //
       var homeHtmlToInsertIntoMainPage = buildAndShowCategoriesHTML(chosenCategoryShortName);
 
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      //insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
